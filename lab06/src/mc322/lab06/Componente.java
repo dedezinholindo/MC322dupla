2d29bddf204a9e0fb60b7b2316409ad6752ddf53
gadelhap <p186985@dac.unicamp.br>
2021-05-06 20:23:39 -0300

    implementei construtores com sobrecarga para Componente

--- lab06/src/mc322/lab06/Componente.java
+++ lab06/src/mc322/lab06/Componente.java
@@ -1,3 +1,27 @@
 package mc322.lab06;
 
-public class Componente {}
+public class Componente {
+    char tipo; /* '-' para vazio, 'P' para herói, 'W' para Wumpus,
+                    'B' para buraco, 'O' para ouro, 'f' para fedor e 'b'  para brisa. */
+    int[] coordenadas; /* tamanho 2. */
+
+    /**
+     * tipo: tipo do componente.
+     * coordenadas: coordenadas do componente.
+     * Inicializa um componente.
+     */
+    Componente (char tipo, int[] coordenadas) {
+        this.tipo = tipo;
+        this.coordenadas = coordenadas.clone;
+    }
+
+    /**
+     * componente: componente na forma ["i:j", "t"], em que i é a linha, j, a
+     * coluna e t o tipo do componente, sendo '_' o tipo vazio.
+     * Inicializa um componente.
+     */
+    Componente (String[] componente) {
+        this((componente[1] == '_') ? '-' : componente[1],
+                Posicao.coordenadasParaInt(componente[0]));
+    }
+}
